b93cc8c900f8f4a589444b551361e3b3c8eabb96
MatDV <matdv@matdvs-macbook-pro.home>
2024-08-26 17:29:24 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+// imports img
+import headerImage from "./assets/images/header-image.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3000/");
+    console.log(response.data);
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <div>
+      {" "}
+      <header>
+        <h2>{data.restaurant.name}</h2>
+        <p>{data.restaurant.description}</p>
+        <img src={headerImage} alt="header-image" />
+      </header>
+      <main>
+        <div>
+          {/* 1er .map qui retourne le titre de la catégorie */}
+          <h2></h2>
+          {/* 2eme .map qui retourne chaque "meal" et ses infos en fonction de la catégorie */}
+          <div>
+            <h3></h3>
+            <p></p>
+            <p></p>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
